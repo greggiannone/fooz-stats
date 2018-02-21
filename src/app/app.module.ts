03128650a2380bfd698373e590f73de00dcbb187
Greg Giannone <greg.giannone@hyland.com>
2018-02-21 10:24:50 -0500

    Added placeholder list and filter bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { GameComponent } from './game/game.component';
 import { MatchComponent } from './match/match.component';
 import { MatchesService } from './services/matches.service';
 import { NameService } from './services/name.service';
+import { MatchListComponent } from './match-list/match-list.component';
+import { FilterBarComponent } from './filter-bar/filter-bar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         GameComponent,
-        MatchComponent
+        MatchComponent,
+        MatchListComponent,
+        FilterBarComponent
     ],
     imports: [
         BrowserModule,
